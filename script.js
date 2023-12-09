1d37db3eda152937b018789c02d198901584d100
Srikanth-madasu <142045491+Srikanth-madasu@users.noreply.github.com>
2023-12-09 21:25:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const extendHex = (shortHex) => {
-  // write your code here
+  // Remove the # if it exists and convert to lowercase
+  shortHex = shortHex.replace(/^#/, "").toLowerCase();
+
+  // Check if the input is a valid short hex code
+  if (!/^[0-9a-f]{3}$/i.test(shortHex)) {
+    console.error("Invalid short hex code");
+    return;
+  }
+
+  // Extend the short hex code to the full hex code
+  const fullHex =
+    shortHex
+      .split("")
+      .map((char) => char.repeat(2))
+      .join("");
+
+  // Add the # prefix and return the full hex code
+  return `#${fullHex}`;
 };
 
 // Do not change the code below.
